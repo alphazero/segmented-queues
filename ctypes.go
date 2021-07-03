e3c2246214ef843e68b0db1f0afcfde5f8849513
Joubin Muhammad Houshyar <jhoushyar@gmail.com>
2021-07-02 22:20:25 -0400

    NOP comment ctypes

--- ctypes.go
+++ ctypes.go
@@ -2,6 +2,7 @@
 
 package segque
 
+// Container types
 type CType int
 
 func (c CType) String() string {
@@ -9,13 +10,13 @@ func (c CType) String() string {
 }
 
 const (
-	_ CType = iota
-	BA
-	Co2_I_C
-	Co2_I_R
-	Co2_II_C
-	Co2_II_R
-	Co2_II_Rand
+	_           CType = iota
+	BA                // basic array with direct addressing
+	Co2_I_C           // single array choice of 2 using container sequence number
+	Co2_I_R           // single array choice of 2 using record sequence number
+	Co2_II_C          // double array choice of 2 using container sequence number
+	Co2_II_R          // double array choice of 2 using record sequence number
+	Co2_II_Rand       // double array choice of 2 with random choice
 )
 
 var ctypes = map[CType]string{
